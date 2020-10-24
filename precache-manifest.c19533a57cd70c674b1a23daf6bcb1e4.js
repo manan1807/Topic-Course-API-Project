f08528ec783e6f2df17c81b6c5ea5779c9869848
manan1807 <mananaggarwal517@gmail.com>
2020-10-24 18:05:37 +0530

    Updates

---
+++ precache-manifest.c19533a57cd70c674b1a23daf6bcb1e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c273774c9597b5659f80967adc418e6",
+    "url": "/react-project-RestAPI-Bootstrap/index.html"
+  },
+  {
+    "revision": "a9b5fefe419257184c6b",
+    "url": "/react-project-RestAPI-Bootstrap/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "195cf9174547b702b75d",
+    "url": "/react-project-RestAPI-Bootstrap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9b5fefe419257184c6b",
+    "url": "/react-project-RestAPI-Bootstrap/static/js/2.3b824040.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-project-RestAPI-Bootstrap/static/js/2.3b824040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195cf9174547b702b75d",
+    "url": "/react-project-RestAPI-Bootstrap/static/js/main.d82303b5.chunk.js"
+  },
+  {
+    "revision": "27e064bb53a63c3a0ec7",
+    "url": "/react-project-RestAPI-Bootstrap/static/js/runtime-main.56141725.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-project-RestAPI-Bootstrap/static/media/logo.5d5d9eef.svg"
+  }
+]);
